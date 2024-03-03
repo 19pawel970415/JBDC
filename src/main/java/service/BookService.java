f124623dc3620e8a97401c888c453404c09f7b24
19Pawel97 <19pawel9772@gmail.com>
2024-03-03 18:13:42 +0100

    Service added

---
+++ src/main/java/service/BookService.java
@@ -0,0 +1,12 @@
+package service;
+
+public class BookService {
+
+//Stworzyć metodę, która zwróci wszystkie książki (listę obiektów) wydane przed zadaną w argumencie datą.
+//Stworzyć metodę, która zwróci wszystkie książki, których autorem jest osoba wskazana jako argument metody.
+//Stworzyć metodę, która zwróci wszystkie książki, których autorem jest osoba wskazana jako argument metody z preparedStatement
+//Stworzyć metodę, która zwróci wszystkie książki posortowane od A do Z po tytule.
+//Stworzyć metodę, która zwróci książkę lub null, na podstawie isbn przekazanego jako parametr metody.
+
+
+}
